f8b1116aa549fdf2dfa30d16a58b0ef46d48c785
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-07 00:43:56 +0800

    Security

---
+++ src/main/java/com/udo/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.udo.configuration;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "jdfnekjdbnfxknvlbljfmsbnfjfjbslfnsjfblwngjrbvkjfhdbvkdjfksnjllflnglsjlrbgjsrfkjdsflkjnjnfbfklj";
+    public static final String JWT_HEADER = "Authorization";
+}
