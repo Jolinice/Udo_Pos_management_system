87edb3f354ba0b00aa7add79b40b75d0054dffde
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-10 19:24:51 +0800

    Security

--- src/main/java/com/udo/payload/response/AuthResponse.java
+++ src/main/java/com/udo/payload/response/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.udo.payload.response;
 
+import com.udo.payload.DTO.UserDTO;
+import lombok.Data;
+
+@Data
 public class AuthResponse {
+    private String jwt;
+    private String message;
+    private UserDTO userDTO;
 }
