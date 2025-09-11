ae55e6433c0939563b7f4f733cb75ce50026d261
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-11 21:45:13 +0800

    Auth & Login

--- src/main/java/com/udo/service/AuthService.java
+++ src/main/java/com/udo/service/AuthService.java
@@ -1,10 +1,11 @@
 package com.udo.service;
 
+import com.udo.exceptions.UserException;
 import com.udo.payload.DTO.UserDTO;
 import com.udo.payload.response.AuthResponse;
 
 public interface AuthService {
-    AuthResponse signup(UserDTO userDTO);
+    AuthResponse signup(UserDTO userDTO) throws UserException;
 
     AuthResponse login(UserDTO userDTO);
 }
