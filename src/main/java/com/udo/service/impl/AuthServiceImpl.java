2b678844220bb36daf4ee6c9d158eb72ceb779e3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-09-12 08:24:57 +0800

    Auth & Login

--- src/main/java/com/udo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/udo/service/impl/AuthServiceImpl.java
@@ -1,15 +1,22 @@
 package com.udo.service.impl;
 
 import com.udo.configuration.JwtProvider;
+import com.udo.domain.UserRole;
 import com.udo.exceptions.UserException;
+import com.udo.mapper.UserMapper;
 import com.udo.model.User;
 import com.udo.payload.DTO.UserDTO;
 import com.udo.payload.response.AuthResponse;
 import com.udo.repository.UserRepository;
 import com.udo.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +32,31 @@ public class AuthServiceImpl implements AuthService {
         if(user != null){
             throw new UserException("Email id already registered");
         }
-        return null;
+        if(userDTO.getRole().equals(UserRole.ROLE_ADMIN)){
+            throw new UserException("Role admin is not allowed");
+        }
+
+        User newUser = new User();
+        newUser.setEmail(userDTO.getEmail());
+        newUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        newUser.setRole(userDTO.getRole());
+        newUser.setFullName(userDTO.getFullName());
+        newUser.setPhone(userDTO.getPhone());
+        newUser.setLastLoginAt(LocalDateTime.now());
+        newUser.setCreateAt(LocalDateTime.now());
+        newUser.setUpdateAt(LocalDateTime.now());
+        User savedUser = userRepository.save(newUser);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Registered Successfully");
+        authResponse.setUserDTO(UserMapper.toDTO(savedUser));
+
+        return authResponse;
     }
 
     @Override
